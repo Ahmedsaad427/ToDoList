94c0ee966620276d303574b02d8261ec67de922e
Ahmed Saad <145069718+Ahmedsaad427@users.noreply.github.com>
2025-01-04 11:55:26 +0200

    Update time.js

--- JS/time.js
+++ JS/time.js
@@ -1,15 +1,2 @@
-function startLiveTime() {
-            // Get the current time
-            let currentTime = new Date();
-
-            // Function to update the displayed time
-            function updateTime() {
-                currentTime.setSeconds(currentTime.getSeconds() + 1); // Increment by 1 second
-                document.getElementById("datetime").innerHTML = currentTime.toLocaleTimeString(); // Display time
-            }
-
-            // Update the time every second
-            setInterval(updateTime, 1000);
-
-            // Display the initial time immediately
-            updateTime();
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleString(); 
